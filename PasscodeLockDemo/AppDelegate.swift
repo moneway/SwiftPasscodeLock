55d9f10a83323a08cc7dc80926c28b67b7ecabec
Romain Vermot <vermot.romain@gmail.com>
2019-06-13 15:19:54 +0200

    Conversion to Swift 5.0

--- PasscodeLockDemo/AppDelegate.swift
+++ PasscodeLockDemo/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return presenter
     }()
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let _ = passcodeLockPresenter
         
